88d51ecd10eb66af6534594fef2dd46810358edc
unknown <maha2691992@gmail.com>
2020-06-08 12:32:35 +0200

    creating test tables

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if (!$conn) {
     exit;
 }
 // sql to create table
-$sql = "CREATE TABLE addressbook (
+$sql1 = "CREATE TABLE addressbook (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) NOT NULL,
 firstname VARCHAR(30) NOT NULL,
@@ -16,25 +16,23 @@ email VARCHAR(30),
 street VARCHAR(50)  NOT NULL,
 zip_code VARCHAR(50)  NOT NULL,
 city VARCHAR(50)  NOT NULL,
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 // sql to create table
-$sql = "CREATE TABLE cities (
+$sql2 = "CREATE TABLE cities (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) NOT NULL,
+created_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
 
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
+if ($conn->query($sql1) === TRUE && $conn->query($sql1) === TRUE  ) {
+    echo "Table created successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
 
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+//echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+//echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
 
 mysqli_close($conn);
 ?>
